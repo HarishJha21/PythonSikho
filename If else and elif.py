209890954f1a982ea8425ba5c3cc1a06ebb7a465
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-05-17 21:51:25 +0530

    Add files via upload

---
+++ If else and elif.py
@@ -0,0 +1,21 @@
+# var1=7
+# var2=67
+# var3=int(input())
+# if var3>var2:
+#     print("Greater")
+# elif var3==var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
+# list1=[5,7,3]
+# print(15 not in list1)
+# if 15 not in list1:
+#     print("No its not in the list")
+print("What is your age?")
+age=int(input())
+if age<18:
+    print("You cannot drive")
+elif age==18:
+    Print("We will think about you")
+else:
+    print("You can drive")
