fe4bbb416ad4b5fd972e1a190d902423a948f935
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-05-17 21:58:32 +0530

    Add files via upload

---
+++ Lambda.py
@@ -0,0 +1,14 @@
+# lambda Function
+# def add(a,b):
+    # return a+b
+# 
+# minus=lambda x,y:x-y
+# 
+# def minus(x,y):
+#     return x-y
+
+# print(minus(10,3))
+
+a=[[1,14], [0,6], [8,23]]
+a.sort(key=lambda x:x[1])
+print(a)
