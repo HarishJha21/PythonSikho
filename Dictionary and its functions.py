a27fc4b228756d70e403f71e89ef8c1d5c615b9e
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-05-17 21:52:44 +0530

    Add files via upload

---
+++ Dictionary and its functions.py
@@ -0,0 +1,13 @@
+d1={}
+# print(type(d1))
+d2={"Anshuman":"Salad", "Shashi":"Chicken",
+"Meet":"Gobhi", "Harish":"Paneer", 
+"Sumit":{"B":"Maar", "L":"Biskuit", "D":"Momos"}}
+# d2["Sakshi"]="Junk Food"
+# d2[236]="Vegg Chillie"
+# del d2[236]
+# print(d2["Sumit"]["B"])
+# d3=d2.copy()
+# del d3["Harish"]
+# print(d2.keys())
+print(d2.items())
