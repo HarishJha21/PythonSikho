ba1cc377fb241f6f273caabd2c228e3b9a334923
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-05-18 18:34:36 +0530

    Add files via upload

---
+++ File IO Basics.py
@@ -0,0 +1,10 @@
+# File IO Basics
+"""
+"r"-Open File for reading-default
+"w"-Open a file for writing
+"x"-Creates file if not exists
+"a"-Add more content to a file
+"t"-text mode-default
+"b"-binary mode
+"+"-read and write
+"""
