e6f599fd320ef231ddebc4449c934f99f490ac92
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-05-18 18:03:34 +0530

    Add files via upload

---
+++ Comments,Escape and Print.py
@@ -0,0 +1,12 @@
+#Kindly ignore this line
+"""
+multi
+line
+ignore
+now
+"""
+"""This is learning
+"""
+print("Kaisa hai anshuman piro", "Anshuman bhai tu stud hai malum na tereko")
+print("C:\Harish")
+print("Anshuman is \n piro boy \t")
