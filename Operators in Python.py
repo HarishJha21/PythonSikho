c42f8aa2eab9a645209fdc0bf04f67083b120264
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-05-18 18:29:21 +0530

    Add files via upload

---
+++ Operators in Python.py
@@ -0,0 +1,49 @@
+# Operators in Python
+# Arithmetic Operator
+# Assignment Operator
+# Comparision Operator
+# Logical Operator
+# Identity Operator
+# Membership Operator
+# Bitwise Operator
+
+# Arithmetic Operator
+# print("5+6 is", 5+6)
+# print("5-6 is", 5-6)
+# print("5*6 is", 5*6)
+# print("5/6 is", 5/6)
+# print("5**3 is", 5**3)
+# print("5%5 is", 5%5)
+# print("15//6 is", 15//6)
+
+# Assignment Operator
+# print("Assignment Operator")
+# x=5
+# print(x)
+# x+=7
+# print(x)
+
+# Comparision Operator
+i=5
+# print(i==5)
+
+# Logical Operator
+a=True
+b=False
+# print(a or b)
+
+# Identity Operator
+# print(5 is not 5)
+
+# Membership Operator
+list=[3,34,45,68,69,212]
+# print(324 not in list)
+
+# Binary Operator
+# 0-00
+# 1-01
+# 2-10
+# 3-11
+print(0 & 2
+)
+print(0|3)
