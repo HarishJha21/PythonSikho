627f60d86faeb781217d1e1d89de69eaf684db66
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-10-08 12:18:34 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,24 @@
+a = 3
+b = 4
+
+# Arithmetic Operators
+print("The value of  3+4 is ", 3+4)
+print("The value of  3-4 is ", 3-4)
+print("The value of  3*4 is ", 3*4)
+print("The value of  3/4 is ", 3/4)
+
+# Assignment Operators
+a = 34
+a -=12
+a *=12
+a /=12
+print(a)
+
+# Comparision Operators
+# b = (15==7)
+# b = (15<=7)
+# b = (15>=7)
+# b = (15<7)
+# b = (15>7)
+b = (15!=7)
+print(b)
