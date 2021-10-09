126644a6f442f2b2b5645ed7d383c06f944637e9
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-10-09 12:13:58 +0530

    Add files via upload

---
+++ strings_slicing.py
@@ -0,0 +1,16 @@
+greeting = "Good morning, "
+# name = "Harish"
+# print(type(name))
+
+# Concatenating two strings
+# c = greeting + name
+# print(c)
+# print(name[1])
+
+# print(name[0:5])
+# print(name[:5]) # is same as name [0:4]
+
+name = "HarishIsGood"
+d = name[0::2]
+print(d)
+
