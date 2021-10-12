7a02cef791188cb847ac634fd17de09d81925ace
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-10-12 12:58:29 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,30 @@
+a = 10
+
+# if-elif-else ladder
+# if(a<5):
+#     print("The value of a is greater than 5")
+# elif(a>15):
+#     print("The value of a is greater than 15")
+# elif(a>9):
+#     print("The value of a is greater than 9")
+# elif(a>19):
+#     print("The value of a is greater than 19")
+# else:
+#     print("The value is not greater than 5 or 9")
+
+# Multiple if statements
+if(a<5):
+    print("The value of a is greater than 5")
+
+if(a>15):
+    print("The value of a is greater than 15")
+    
+if(a>9):
+    print("The value of a is greater than 9")
+
+if(a>19):
+    print("The value of a is greater than 19")
+else:
+    print("The value is not greater than 5 or 9")
+
+print("Done")
