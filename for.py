6d561f02f83180d6650eefb63b00b505e814f622
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-10-13 14:40:54 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,13 @@
+# fruits = ['Banana', 'Watermelon', 'Grapes', 'Mangoes']
+# for item in fruits:
+#     print(item)
+   
+# Range functions:
+# for i in range(1,8, 2):
+#     print(i)
+
+# For loop with else:
+for i in range(10):
+    print(i)
+else:
+    print("This is inside else of for")
